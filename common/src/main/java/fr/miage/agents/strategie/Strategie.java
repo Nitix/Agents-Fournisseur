6351dc1820f41376c543d4ee01878918a084bbb3
ArthurMourey <mourey.arthur@gmail.com>
2016-11-21 09:39:26 +0100

    Negociation

--- common/src/main/java/fr/miage/agents/strategie/Strategie.java
+++ common/src/main/java/fr/miage/agents/strategie/Strategie.java
@@ -12,12 +12,13 @@ import org.hibernate.query.Query;
 import java.util.Iterator;
 import java.util.List;
 
+
 /**
  * Created by Arthur on 15/11/2016.
  */
 public class Strategie {
 
-    public float venteProduit(String nomProduit, String marqueProduit, int quantiteDemandee){
+    public static float venteProduit(String nomProduit, String marqueProduit, int quantiteDemandee){
         Query query= HibernateUtil.openSession().createQuery("from Produit where nomProduit=:name and marqueProduit=:mrq");
         query.setParameter("name", nomProduit);
         query.setParameter("mrq", marqueProduit);
@@ -37,7 +38,7 @@ public class Strategie {
         return prixVente*quantiteDemandee;
     }
 
-    public float getPrixStrategiqueByQuantiteStockee(Produit p){
+    public static float getPrixStrategiqueByQuantiteStockee(Produit p){
 
         int qteStockee = p.getQuantiteProduit();
 
@@ -48,15 +49,34 @@ public class Strategie {
         return prix;
     }
 
-    public float getPrixStrategiqueByQuantiteDemandee(Produit p, int quantiteDemandee){
+    public static float getPrixStrategiqueByQuantiteDemandee(Produit p, int quantiteDemandee){
         float pourcentageQuantite = ((quantiteDemandee*100)/p.getQuantiteProduit());
         return p.getPrixProduit()+((100*20/p.getPrixProduit())/(2*pourcentageQuantite));
     }
 
-    public boolean assezQuantite(Produit p, int quantite){
+    public static boolean assezQuantite(Produit p, int quantite){
         if(p.getQuantiteProduit()>=quantite){
             return true;
         }
         return false;
+    }
+
+    public static boolean accepterRefuserNegociation(){
+        int reussite = (int)Math.random()*100;
+        if(reussite>=20){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+    public static float calculPrixApresNegociation(float prixCalcule){
+        if(accepterRefuserNegociation()){
+            return prixCalcule - ((prixCalcule*20)/100);
+        }
+        else{
+            return prixCalcule;
+        }
     }
 }
