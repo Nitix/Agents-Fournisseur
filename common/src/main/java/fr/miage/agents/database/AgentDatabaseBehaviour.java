f5dc665cdbcf96deb11774fc0a27f4370f045f20
ArthurMourey <mourey.arthur@gmail.com>
2016-11-25 10:01:30 +0100

    Envois réponse achat

--- common/src/main/java/fr/miage/agents/database/AgentDatabaseBehaviour.java
+++ common/src/main/java/fr/miage/agents/database/AgentDatabaseBehaviour.java
@@ -7,6 +7,7 @@ import fr.miage.agents.api.message.reponse.ResultatRecherche;
 import fr.miage.agents.fournisseur.model.Categorie;
 import fr.miage.agents.fournisseur.model.Produit;
 import fr.miage.agents.strategie.Strategie;
+import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.Behaviour;
 import jade.core.event.MessageAdapter;
@@ -15,6 +16,8 @@ import jade.lang.acl.MessageTemplate;
 import jade.lang.acl.UnreadableException;
 import org.json.JSONArray;
 import org.json.JSONObject;
+
+import java.io.IOException;
 
 /**
  * Created by Alexandre on 11/11/2016.
@@ -38,7 +41,13 @@ public class AgentDatabaseBehaviour extends Behaviour {
                 case Achat:
                     Acheter achat = (Acheter) m;
                     Message reponse = traitementAchat(achat);
-                    //Send reponse
+                    ACLMessage msgReponse = new ACLMessage(ACLMessage.INFORM);
+                    msgReponse.addReceiver(new AID("magasinier", AID.ISLOCALNAME));
+                    try {
+                        msgReponse.setContentObject(reponse);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                     break;
                 case Aide:
                     Aide aide = (Aide) m;
@@ -62,27 +71,6 @@ public class AgentDatabaseBehaviour extends Behaviour {
         } catch (UnreadableException e) {
             e.printStackTrace();
         }
-        /*
-
-        JSONObject obj = new JSONObject(msg.getContent());
-        String action = obj.getString("action");
-        Integer idCat = Categorie.addCategorie("Légume");
-        Produit.addProduit("Tomate", "J'aime les tomates",1.0f,1 ,idCat, "Bonduel");
-        Produit.addProduit("Pomme", "J'aime les pommes",1.0f,1 ,idCat, "Granny");
-        Produit.listProduits();
-        switch (action){
-            case "Produire":
-                JSONArray arr = obj.getJSONArray("elements");
-                for (int i = 0; i < arr.length(); i++) {
-                   JSONObject ligne = arr.getJSONObject(i);
-                    String nom = ligne.getString("nom");
-                    String marque = ligne.getString("marque");
-                    int quantite = ligne.getInt("quantite");
-                    Produit.addQuantity(nom, marque, quantite);
-                }
-        }
-
-        System.out.println("Le message reçu : " + msg.getContent());*/
     }
 
     public boolean done() {
